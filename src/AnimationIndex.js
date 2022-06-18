abe4f435ad35c84762a2ca98765e1147ed0c1fa6
MacMini <macmini@MacMinis-Mac-mini.local>
2022-06-18 23:34:56 +0530

    Circle animatin completed with seInterval JS method

--- src/AnimationIndex.js
+++ src/AnimationIndex.js
@@ -36,54 +36,71 @@ import {
 
 const Animation=()=>{
 
-  const c="10%";
-  const y="20%";
-  const radius="700";
+  const c="50%";
+  const y="50%";
+  const radius=40;
   const color="white"
   const stroke="red"
-  const strokeWidth=9;
-  const speed=20000;
+  const strokeWidth=10;
+  const speed=60;
 const circumference=2*Math.PI*radius;
 const halfCircle=radius+strokeWidth;
-const  [percent,setPercent]=useState(1);
+const  [percent,setPercent]=useState(5);
+
+
+
 
 useEffect(()=>{
   let id;
-// setInterval(()=>{
-//  id=setPercent((pre)=>{
-//   if(pre==10)return 1;
-//   return pre+1;
-// })
-// },speed)
+setInterval(()=>{
+ id=setPercent((pre)=>{
+  if(pre==100){
+     clearInterval(id)
+    return 1;
+  }
+  return pre+1;
+})
+},speed)
 
 return ()=>{
 clearInterval(id);
 }
 },[])
+ 
+    function calPercent(percentNo){
+   
+      return circumference-(circumference*percentNo)/100;
+    }
+    // style={{borderColor:"red",borderWidth:1}}
   return(
-    <View style={{borderWidth:2,borderColor:'black',width:"100%",height:"100%"}}>
-      
-     <Svg height={radius*2} width={radius*2} viewBox={`0 0  ${halfCircle*2} ${halfCircle*2}` } >
+    <View  style={{width:'100%',borderColor:"black",borderWidth:3,height:"100%",justifyContent:'center',alignItems:'center'}}>
+     
+     <Svg style={{borderColor:"red",borderWidth:1,position:"relative"}}  height={radius*2} width={radius*2}  viewBox={`0 0  ${halfCircle*2} ${halfCircle*2}`}     >
+     <Text style={{position:'absolute',top:30,textAlign:"center",width:"100%"}}>{percent}</Text>
+         <G> 
           <Circle
             cx={c}
             cy={y}
             r={radius}
             stroke={stroke}
             strokeWidth={strokeWidth}
-            fill={color}
+            fill="transparent"
             strokeOpacity="0.2"
-          />
+            
+          ></Circle>
+           
           <Circle
             cx={c}
             cy={y}
             r={radius}
             stroke={stroke}
             strokeWidth={strokeWidth}
-            fill={color}
+            fill="transparent"
           strokeDasharray={circumference}
-          strokeDashoffset={circumference/3}
+          strokeDashoffset={calPercent(percent)}
+          strokeLinecap="round"
           />
-
+        </G>
           {/* <Rect
             x="15"
             y="15"
